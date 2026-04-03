e35cd2ac1404f7af2f8bc51ffa7f471380b6873a
WilliamHarry168 <william.felix059@gmail.com>
2026-04-03 18:17:44 +0700

    Bypass CORS

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/WilliamFelix168/learning-journey/tree/main/Golang/WPU/Project/project-management/routes"
 	"github.com/WilliamFelix168/learning-journey/tree/main/Golang/WPU/Project/project-management/services"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -19,6 +20,14 @@ func main() {
 	seed.SeedAdmin()
 
 	app := fiber.New()
+
+	//Agar bisa bypass di FE
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5173",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+	}))
 
 	//User
 	userRepo := repositories.NewUserRepository()
